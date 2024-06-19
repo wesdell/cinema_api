ba5547bd53c4e06d441f598819dfa692af6a83ac
Wesdell <wesdell17@gmail.com>
2024-06-18 19:25:44 -0500

    New app DBContext to create database tables

---
+++ cinema_api/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using cinema_api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace cinema_api
+{
+	public class ApplicationDBContext : DbContext
+	{
+		public ApplicationDBContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Genre> Genre { get; set; }
+	}
+}
