d1c5cfcb8a313f699adebece705d0761e0614342
Wesdell <wesdell17@gmail.com>
2024-06-18 19:08:41 -0500

    New Genre Entity to record movies genre

---
+++ cinema_api/Entities/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cinema_api.Entities
+{
+	public class Genre
+	{
+		public int Id { get; set; }
+		[Required]
+		[StringLength(50)]
+		public string Name { get; set; }
+	}
+}
