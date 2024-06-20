de26b1d8731df4af40267fc3ac8ae787d4cf28da
Wesdell <wesdell17@gmail.com>
2024-06-19 19:58:42 -0500

    New DTO to receive Genre data

---
+++ cinema_api/DTOs/CreateGenreDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cinema_api.DTOs
+{
+	public class CreateGenreDTO
+	{
+		[Required]
+		[StringLength(50)]
+		public string Name { get; set; }
+	}
+}
