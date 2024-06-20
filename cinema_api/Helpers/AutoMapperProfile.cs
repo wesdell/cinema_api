7036cb427f3f56e7654bfea0c03b3d9dd193be38
Wesdell <wesdell17@gmail.com>
2024-06-19 19:37:50 -0500

    Add new mapper for Genre

---
+++ cinema_api/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using cinema_api.DTOs;
+using cinema_api.Entities;
+
+namespace cinema_api.Helpers
+{
+	public class AutoMapperProfile : Profile
+	{
+		public AutoMapperProfile()
+		{
+			CreateMap<Genre, GenreDTO>().ReverseMap();
+		}
+	}
+}
