b48fc4681d75e9755ba794094352d24273a8b267
Wesdell <wesdell17@gmail.com>
2024-06-20 20:06:54 -0500

    New DTO to create an Actor

---
+++ cinema_api/DTOs/CreateActorDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cinema_api.DTOs
+{
+	public class CreateActorDTO
+	{
+		[Required]
+		[StringLength(150)]
+		public string Name { get; set; }
+		public DateTime Birthday { get; set; }
+	}
+}
