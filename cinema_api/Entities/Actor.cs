2544214ee1253d3d8f1ae467f616d6af24a9f746
Wesdell <wesdell17@gmail.com>
2024-06-20 19:53:12 -0500

    Add Actor entity to record Actor data

---
+++ cinema_api/Entities/Actor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cinema_api.Entities
+{
+	public class Actor
+	{
+		public int Id { get; set; }
+		[Required]
+		[StringLength(150)]
+		public string Name { get; set; }
+		public string ImageURL { get; set; }
+		public DateTime Birthday { get; set; }
+	}
+}
