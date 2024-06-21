b63b5ddd81e49228131d7abd9b29700047bec4b9
Wesdell <wesdell17@gmail.com>
2024-06-20 19:55:34 -0500

    New endpoint to get all actors

---
+++ cinema_api/Controllers/ActorController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using cinema_api.DTOs;
+using cinema_api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace cinema_api.Controllers
+{
+	[ApiController]
+	[Route("api/actor")]
+	public class ActorController : ControllerBase
+	{
+		private readonly ApplicationDBContext _applicationContext;
+		private readonly IMapper _mapper;
+
+		public ActorController(ApplicationDBContext applicationDBContext, IMapper mapper)
+		{
+			_applicationContext = applicationDBContext;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<List<ActorDTO>>> GetAll()
+		{
+			List<Actor> authors = await _applicationContext.Actor.ToListAsync();
+
+			List<ActorDTO> authorDTOs = _mapper.Map<List<ActorDTO>>(authors);
+			return authorDTOs;
+		}
+	}
+}
