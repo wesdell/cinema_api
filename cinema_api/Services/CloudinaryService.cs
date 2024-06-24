db28f54ebbca9a2e2555c6428ab18bd49668b44e
Wesdell <wesdell17@gmail.com>
2024-06-24 17:30:49 -0500

    New service to upload an image to Cloudinary

---
+++ cinema_api/Services/CloudinaryService.cs
@@ -0,0 +1,43 @@
+﻿using cinema_api.Utils;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Options;
+
+namespace cinema_api.Services
+{
+	public class CloudinaryService
+	{
+		private readonly Cloudinary _cloudinary;
+
+		public CloudinaryService(IOptions<CloudinarySettings> config)
+		{
+			var account = new Account(
+				config.Value.CloudName,
+				config.Value.ApiKey,
+				config.Value.ApiSecret
+			);
+
+			_cloudinary = new Cloudinary(account);
+		}
+
+		public async Task<ImageUploadResult> UploadImageAsync(IFormFile file)
+		{
+			var uploadResult = new ImageUploadResult();
+
+			if (file.Length > 0)
+			{
+				using (var stream = file.OpenReadStream())
+				{
+					var uploadParams = new ImageUploadParams()
+					{
+						File = new FileDescription(file.FileName, stream)
+					};
+					uploadResult = await _cloudinary.UploadAsync(uploadParams);
+				}
+			}
+
+			return uploadResult;
+		}
+	}
+
+}
