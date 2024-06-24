d185665692253ea949dbb3bd30951a7579fcc35d
Wesdell <wesdell17@gmail.com>
2024-06-24 17:59:08 -0500

    New DTO to get partial changes in Actor data

---
+++ cinema_api/DTOs/PatchActorDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cinema_api.DTOs
+{
+	public class PatchActorDTO
+	{
+		[Required]
+		[StringLength(150)]
+		public string Name { get; set; }
+		public DateTime Birthday { get; set; }
+	}
+}
