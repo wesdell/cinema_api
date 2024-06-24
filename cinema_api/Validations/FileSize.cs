0b02bb61f6fdabf7ea2a38f97ee397a2354b47fc
Wesdell <wesdell17@gmail.com>
2024-06-23 21:21:48 -0500

    New file size validation attribute

---
+++ cinema_api/Validations/FileSize.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cinema_api.Validations
+{
+	public class FileSize : ValidationAttribute
+	{
+		private readonly int fileMaxSizeMB;
+
+		public FileSize(int fileMaxSizeMB)
+		{
+			this.fileMaxSizeMB = fileMaxSizeMB;
+		}
+
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			if (value == null)
+			{
+				return ValidationResult.Success;
+			}
+
+			IFormFile file = value as IFormFile;
+
+			if (file == null)
+			{
+				return ValidationResult.Success;
+			}
+
+			if (file.Length > fileMaxSizeMB * 1024 * 1024)
+			{
+				return new ValidationResult($"The image size must not exceed {fileMaxSizeMB} MB.");
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+}
