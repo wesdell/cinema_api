3fbb99d273c5d08e740d1d8a1b56ac2148c32600
Wesdell <wesdell17@gmail.com>
2024-06-25 17:18:40 -0500

    New DTO to track pagination

---
+++ cinema_api/DTOs/PaginationDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace cinema_api.DTOs
+{
+	public class PaginationDTO
+	{
+		public int Page { get; set; } = 1;
+		public int RecordsPerPage { get; set; } = 10;
+		public int MaxRecordsPerPage { get; set; } = 20;
+
+		public int RecordsToShowPerPage
+		{
+			get
+			{
+				return RecordsPerPage;
+			}
+			set
+			{
+				RecordsPerPage = (value > MaxRecordsPerPage) ? MaxRecordsPerPage : value;
+			}
+		}
+	}
+}
