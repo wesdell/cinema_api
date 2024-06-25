bff64bf1f61f4988f9a842dd888742b5eb1ae16f
Wesdell <wesdell17@gmail.com>
2024-06-25 17:19:25 -0500

    Set up pagination parameter in header

---
+++ cinema_api/Helpers/HttpContextExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cinema_api.Helpers
+{
+	public static class HttpContextExtensions
+	{
+		public async static Task InsertPaginationParameters<T>(this HttpContext httpContext, IQueryable<T> queryable, int recordsPerPage)
+		{
+			double amount = await queryable.CountAsync();
+			double pageAmount = Math.Ceiling(amount / recordsPerPage);
+			httpContext.Response.Headers.Append("recordsAmount", pageAmount.ToString());
+		}
+	}
+}
