5e4a84b2d2ba20b27f2c2ce4beb36eded6b95fc7
Wesdell <wesdell17@gmail.com>
2024-06-25 17:20:04 -0500

    Code pagination algorithm

---
+++ cinema_api/Helpers/QueryableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using cinema_api.DTOs;
+
+namespace cinema_api.Helpers
+{
+	public static class QueryableExtensions
+	{
+		public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable, PaginationDTO paginationDTO)
+		{
+			return queryable.Skip((paginationDTO.Page - 1) * paginationDTO.RecordsToShowPerPage).Take(paginationDTO.RecordsToShowPerPage);
+		}
+	}
+}
