416745b6a91f79e14cff9ae73462cba4dd013d1c
Wesdell <wesdell17@gmail.com>
2024-06-27 18:54:09 -0500

    New Movie DTO

---
+++ cinema_api/DTOs/MovieDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cinema_api.DTOs
+{
+	public class MovieDTO
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public bool OnBillboard { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		public string PosterURL { get; set; }
+	}
+}
