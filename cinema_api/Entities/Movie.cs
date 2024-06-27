008cf958d254d7ea2973e95c6b119437f6c5d5ae
Wesdell <wesdell17@gmail.com>
2024-06-27 18:41:43 -0500

    New Movie Entity

---
+++ cinema_api/Entities/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cinema_api.Entities
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		[Required]
+		[StringLength(150)]
+		public string Name { get; set; }
+		public bool OnBillboard { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		public string PosterURL { get; set; }
+	}
+}
