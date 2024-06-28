d3b0cd8b8489432b8fa74b0b663e1b9d59087fd1
Wesdell <wesdell17@gmail.com>
2024-06-27 19:17:22 -0500

    New DTO to record new user data at updating a movie

---
+++ cinema_api/DTOs/UpdateMovieDTO.cs
@@ -0,0 +1,19 @@
+﻿using cinema_api.Interfaces;
+using cinema_api.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace cinema_api.DTOs
+{
+	public class UpdateMovieDTO
+	{
+		public int Id { get; set; }
+		[Required]
+		[StringLength(150)]
+		public string Name { get; set; }
+		public bool OnBillboard { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		[FileSize(fileMaxSizeMB: 4)]
+		[FileType(validFileTypes: EFileType.Image)]
+		public IFormFile Poster { get; set; }
+	}
+}
