dd7abc7a1e1d0db52a4a48f31e4e1701d7ce2daa
Wesdell <wesdell17@gmail.com>
2024-06-29 16:31:53 -0500

    Set up MovieGenre table and its key

--- cinema_api/ApplicationDBContext.cs
+++ cinema_api/ApplicationDBContext.cs
@@ -9,8 +9,16 @@ namespace cinema_api
 		{
 		}
 
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<MovieGenre>().HasKey(opt => new { opt.MovieId, opt.GenreId });
+
+			base.OnModelCreating(modelBuilder);
+		}
+
 		public DbSet<Genre> Genre { get; set; }
 		public DbSet<Actor> Actor { get; set; }
 		public DbSet<Movie> Movie { get; set; }
+		public DbSet<MovieGenre> MovieGenre { get; set; }
 	}
 }
